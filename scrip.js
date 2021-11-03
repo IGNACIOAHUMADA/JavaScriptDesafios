a3231d91853eda820df26036b39aa18b4549b6cb
igna ahumada <igna.ahumadaro@gmail.com>
2021-11-02 21:06:25 -0300

    desafio 1 JS

---
+++ scrip.js
@@ -0,0 +1,12 @@
+let nombre = prompt('Ingrese su nombre');
+let suNombre =  " " + "Bienvenido" + " " + nombre;
+alert(suNombre);
+let mensaje = "bienvenido a Js";
+console.log(mensaje +" " + nombre );
+
+let numero = parseInt(prompt('Ingrese un numero'));
+let nume1 = parseInt(10)
+let suma =  numero + nume1 ;
+console.log(suma);
+alert(suma);
+
