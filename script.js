4c11d406fd3a37c3dbc07c9db9a27f90fe7fa26a
igna ahumada <igna.ahumadaro@gmail.com>
2021-11-09 18:09:00 -0300

    desafio 3

---
+++ script.js
@@ -0,0 +1,9 @@
+let ingreseNumero = parseInt(prompt("Ingrese un numero"));
+
+for (let i = 0; i <= ingreseNumero; i++){
+    if(i === 11) {
+        
+        break
+    }
+    console.log("tu tienes " + i )
+}           
