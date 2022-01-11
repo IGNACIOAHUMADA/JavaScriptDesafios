545ba9f719c5fd93e301eb76060c63f98a462b10
igna ahumada <igna.ahumadaro@gmail.com>
2022-01-10 23:57:16 -0300

    entrega final

--- js/carrito.js
+++ js/carrito.js
@@ -1,26 +1,39 @@
+let carrito = []
+let dataCarrito = localStorage.getItem("carrito");
 
-$(() => {
-    let carrito = []
-    let dataCarrito = localStorage.getItem("carrito");
-    
-    if(dataCarrito) carrito = JSON.parse(dataCarrito)
-
-    let stringHTML = "<div class='cardProductos'>";
-    for (const data of carrito) {
-        stringHTML += `
+if(dataCarrito) {
+    carrito = JSON.parse(dataCarrito)
+    mostrarCarrito()
+}
+function mostrarCarrito(){
+    document.getElementById("seleccionProductos").innerHTML = ""
+    for(const data of carrito){
+        $('#seleccionProductos').append( `
         <div class="card" style="width: 18rem;" >
-        <img src="${data.imagen}" class="card-img-top" alt="${data.nombre}"></img>
-        <div class="card-body">
-            <h5 class="card-title">${data.nombre}</h5>
-            <p class="card-text">Unidades seleccionadas(${data.cantidad})</p>
-            <p class="card-text">Productos disponibles(${data.stock})</p>
-            <p class="card-text"><b>$ ${data.precio}</b></p>
-            <button id="${data.idProducto}" class="btn btn-primary delete">🗑️</button>
-        </div>
-    </div>`
+            <img src="${data.imagen}" class="card-img-top" alt="${data.nombre}"></img>
+            <div class="card-body">
+                <h5 class="card-title">${data.nombre}</h5>
+                <p class="card-text">Unidades seleccionadas(${data.cantidad})</p>
+                <p class="card-text">Productos disponibles(${data.stock})</p>
+                <p class="card-text"><b>$ ${data.precio}</b></p>
+                <button id="${data.idProducto}eliminar" class="btn btn-primary delete">🗑️</button>
+            </div>
+        </div>`)
+
+        document.getElementById(`${data.idProducto}eliminar`).addEventListener('click', () => eliminar(data))
     }
-    stringHTML += "</div>"
-    $("#seleccionProductos").html(stringHTML)
-})
+
+}
 
 
+function eliminar(producto){
+    const eliminado = carrito.find(el => el.idProducto === producto.idProducto)
+    if(eliminado.cantidad > 1 ){
+        eliminado.cantidad--
+    }else{
+        carrito = carrito.filter(el => el.idProducto !== producto.idProducto)
+    }
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    mostrarCarrito()
+
+}
--- js/productos.js
+++ js/productos.js
@@ -22,52 +22,63 @@ class Productos {
     sumaIva() {
         this.precio = this.precio * 1.21;
     }
+    
 }
-$.getJSON('http://127.0.0.1:5501/js/stock.json', function(datita){
-    datita.forEach(element => tienda.push(element) );
-    mostrarProductos();
+
+$.getJSON('../data/stock.json', function(datita){
+    if(datita){
+        for(const product of datita){
+            tienda.push(new Productos(product.idProducto, product.nombre, product.precio, product.imagen, product.stock))
+        }
+        for (const Productos of tienda) {
+            Productos.sumaIva();
+        }
+        mostrarProductos();
+    }
 })
+for (const Productos of tienda) {
+    Productos.sumaIva();
+}
 
 function mostrarProductos(){
-for (const producto of tienda) {
-    $('#tienda').append(`
-    <div class="card" style="width: 18rem;" >
-        <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}"></img>
-        <div class="card-body">
-            <h5 class="card-title">${producto.nombre}</h5>
-            <p class="card-text"><b>$ ${producto.precio}</b></p>
-            <button id="${producto.idProducto}" class="btn btn-primary">Agregar al carrito</button>
+    for (const producto of tienda) {
+        $('#tienda').append(`
+        <div class="card" style="width: 18rem;" >
+            <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}"></img>
+            <div class="card-body">
+                <h5 class="card-title">${producto.nombre}</h5>
+                <p class="card-text"><b>$ ${producto.precio}</b></p>
+                <button id=${producto.idProducto} class="btn btn-primary">Agregar al carrito</button>
+            </div>
         </div>
-    </div>
-    `)
-    document.getElementById(`${producto.idProducto}`).addEventListener('click', () => comprarProductos(producto));
-}
+        `)
+        document.getElementById(`${producto.idProducto}`).addEventListener('click', () => comprarProductos(producto));
+    }
 }
 
 function comprarProductos(producto) {
-    let comprar = carrito.find(el => el.nombre === producto.nombre)
+    let comprar = carrito.find(el => el.idProducto === producto.idProducto)
     if (comprar) {
         if (comprar.cantidad < producto.stock) {
             comprar.aumentarCantidad();
-        } 
+        }
     } else {
-        carrito.push(producto);
         producto.aumentarCantidad();
-    } 
+        carrito.push(producto);
+    }
 
     let total = 0;
-        for (let i = 0; i < carrito.length; i++) {
-            total += carrito[i].cantidad;
-        }
-    const compraTotal = document.getElementById('compraTotal');
+    for (let i = 0; i < carrito.length; i++) {
+        total += carrito[i].cantidad;
+    }
+    const compraTotal = document.getElementById('totalCompra');
     compraTotal.innerHTML = total;
     localStorage.setItem('carrito', JSON.stringify(carrito));
-    mostrarProductos();
 }
 
 function cargarLocalStorage() {
     let shop = JSON.parse(localStorage.getItem('carrito'))
-    localStorage.setItem('carrito', shop)
+    
     if (shop) {
         for (let i = 0; i < shop.length; i++) {
             carrito.push(new Productos(shop[i].idProducto, shop[i].nombre, shop[i].precio, shop[i].imagen,shop[i].stock, shop[i].cantidad))
@@ -76,11 +87,9 @@ function cargarLocalStorage() {
         for (let i = 0; i < carrito.length; i++) {
             total += carrito[i].cantidad;
         }
-        const compraTotal = document.getElementById('compraTotal');
+        const compraTotal = document.getElementById('totalCompra');
         compraTotal.innerHTML = total;
         localStorage.setItem('carrito', JSON.stringify(carrito));
     }
 }
 cargarLocalStorage(); 
-
-
